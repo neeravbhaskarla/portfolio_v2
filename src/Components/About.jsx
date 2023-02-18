1edc3bb7841b6df07db0049e8fb4e1b898812107
neeravbhaskarla <ananthaneerav@gmail.com>
2023-02-18 11:55:48 +0530

    fixed navbar not declared issue and navbar links width issue

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import NavBar from './NavBar'
 import '../styles/About.scss'
 
 function About() {
   return (
     <>
       <div className="about-wrapper">
+        <NavBar/>
         <h1>About</h1>
       </div>
     </>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import NavBar from './NavBar'
 import '../styles/Projects.scss'
 
 function Projects() {
   return (
     <>
       <div className="projects-wrapper">
+        <NavBar/>
         <h1>Projects</h1>
       </div>
     </>
