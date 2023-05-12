72c00aa6d4fa022c36ad17d3a0e3299df0a7a802
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2023-05-12 13:45:08 +0530

    Update Experience.js

--- src/Data/Experience.js
+++ src/Data/Experience.js
@@ -10,7 +10,7 @@ export const experiences = [
         id: 'exp-2',
         periodStart: '2022',
         periodEnd: 'Present',
-        name: 'ASSISTANT SYSTEMS ENGINEER',
-        description: 'WORKING AS ASE IN TCS'
+        name: 'FREELANCING',
+        description: 'WORKING ON DIFFERENT PROJECTS INVOLVING TECHNOLOGIES SUCH AS REACTJS, HTML, CSS, JAVASCRIPT, WORDPRESS AND OTHER JAVASCRIPT FRAMEWORKS'
     }
 ]
